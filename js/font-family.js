239c241c065e062a55142982b22d7414ff4551a7
ynxtzi <42765501+ynxtzi@users.noreply.github.com>
2020-06-07 02:48:45 +0800

    Add files via upload

---
+++ js/font-family.js
@@ -0,0 +1,37 @@
+var testContent =
+	"ABCDEFG HIJKLMN</br>OPQ RST UVW XYZ</br>abcdefg hijklmn</br>opq rst uvw xyz</br>0123456789</br>零壹贰叁肆伍陆柒捌玖";
+
+$(() => {
+	var app = new Vue({
+		el: "#app",
+		data: {
+			familys: [],
+			testContent: testContent
+		},
+		methods: {
+			init() {
+				let that = this;
+				$.get('json/default-family.json', function(data) {
+					that.familys = data;
+				});
+			},
+			change(event) {
+				this.testContent = event.currentTarget.innerHTML;
+			},
+			copy(data) {
+				let url = data;
+				let oInput = document.createElement('input');
+				oInput.value = url;
+				document.body.appendChild(oInput);
+				oInput.select();
+				document.execCommand("Copy");
+				new $.zui.Messager('提示消息：复制成功', {
+					placement: 'top-left',
+					type: 'success'
+				}).show();
+				oInput.remove()
+			},
+		}
+	});
+	app.init();
+})
